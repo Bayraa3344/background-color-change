68db7b82a1aae36fd903f38b5be8c030f2af22a3
Bayraa <bayraal3344@gmail.com>
2020-09-07 20:42:27 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let colors = ["red", "gray", "blue", "yellow"];
+i = 0;
+function changeColor() {
+  document.body.style.backgroundColor = colors[i];
+  i++;
+  if (i === colors.length) {
+    i = 0;
+  }
+}
+function changeInpColor() {
+  var newColor = document.getElementById("inp").value;
+  document.body.style.backgroundColor = newColor;
+  colors.push(newColor);
+
+  console.log(colors);
+}
